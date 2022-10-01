c27e153839792cc317f6d55d26574688bfb9004f
DexMP <matveymp@yandex.ru>
2022-10-01 15:42:03 +0300

    Initial commit

---
+++ app/src/main/java/com/raspopova/gol/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.raspopova.gol
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        splash_logo.alpha = 0f
+        splash_logo.animate().setDuration(3000).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
